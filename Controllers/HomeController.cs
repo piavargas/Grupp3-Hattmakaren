f076203e8e40f777908130d6c1eee0a88f4a8b25
eliasaphrem <elias_kaka@hotmail.com>
2024-04-09 14:26:56 +0200

    Lägg till produkt klar
    Går att lägga till produkter klart, dock inte ta bort.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,5 +36,7 @@ namespace Grupp3Hattmakaren.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Grupp3Hattmakaren.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grupp3Hattmakaren.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HatContext _context;
+
+        public ProductController(HatContext context)
+        {
+           
+            _context = context;
+        }
+     
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        { 
+        Product product = new Product();
+            return RedirectToAction("Index", "Home");        
+          }
+        [HttpPost]
+        public IActionResult Add(Product produktObjekt)
+        {
+            if (ModelState.IsValid) // Kontrollera om modellen är giltig
+            {
+                _context.Add(produktObjekt); // Lägg till produkten i databasen
+                _context.SaveChanges(); // Spara ändringar i databasen
+                return RedirectToAction("Index" , "Home"); // Omdirigera till listan över produkter
+            }
+            // Om modellen inte är giltig, visa samma vy med felmeddelanden
+            return View(produktObjekt);
+        }
+       
+       
+
+    }
+}
