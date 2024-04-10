0829efe44e8416a92462d690334186b27387128e
maxhedstrom <max.hedstrom@icloud.com>
2024-04-10 14:46:42 +0200

    Lagt till inventory grund
    Lägg till och visa matieral under inventory fliken, ej frontend fixad

---
+++ Controllers/MaterialController.cs
@@ -0,0 +1,41 @@
+﻿using Grupp3Hattmakaren.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grupp3Hattmakaren.Controllers
+{
+    public class MaterialController : Controller
+    {
+        private readonly HatContext _hatcontext;
+
+        public MaterialController(HatContext _context)
+        {
+            _hatcontext = _context;
+        }
+        public IActionResult Material()
+        {
+            List<Material> materials = _hatcontext.Materials.ToList();
+            ViewBag.MaterialList = materials;  
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Material(Material material) 
+        {
+            var newMaterial = new Material() 
+            { 
+                name = material.name,
+                supplier = material.supplier,
+                price = material.price,
+                quantity = material.quantity
+            };
+
+            _hatcontext.Add(newMaterial);
+            _hatcontext.SaveChanges();
+
+            return RedirectToAction("Material");
+        }
+
+
+
+    }
+}
