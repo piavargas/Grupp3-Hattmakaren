127acefec842d3d3aa24e5f38d5980690cb9b9c1
piavargas <piavargascarrasco@gmail.com>
2024-04-11 13:01:06 +0200

    Test kommentar

--- Models/EnquiryViewModel.cs
+++ Models/EnquiryViewModel.cs
@@ -1,6 +1,21 @@
 ﻿namespace Grupp3Hattmakaren.Models
 {
-    public class CustomerOrderFormViewModel
+    //Kkdfjifheiofnowehfvo
+    public class EnquiryViewModel
     {
+        public bool consentHat { get; set; }
+        public string description { get; set; }
+        public string referenceImage { get; set; }
+        public string addressType { get; set; }
+        public string streetName { get; set; }
+        public int zipCode { get; set; }
+        public string cityName { get; set; }
+        public string countryName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string headSize { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
     }
 }
