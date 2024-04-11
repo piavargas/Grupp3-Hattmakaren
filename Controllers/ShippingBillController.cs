07f7f80da9431034e3bc71b2d656caa110a9e9d8
dakipazar <132764398+dakipazar@users.noreply.github.com>
2024-04-11 15:08:59 +0200

    Börjat med fraktsedel
    Skapat ViewModel och Controller. Även skapat en view som inte har något innehåll än - denna ska öppnas när man är inne på en specifik order och klickar "skriv ut fraktsedel" eller "skapa fraktsedel"

---
+++ Controllers/ShippingBillController.cs
@@ -0,0 +1,48 @@
+﻿using Grupp3Hattmakaren.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace Grupp3Hattmakaren.Controllers
+{
+    public class ShippingBillController : Controller
+    {
+        private readonly HatContext _hatcontext;
+
+        public ShippingBillController(HatContext context)
+        {
+            _hatcontext = context;
+        }
+
+        // GET: ShippingBill/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest(); // Inget ID = Returnera HTTP 400 Bad Request
+            }
+
+            //LINQ query för att extrahera den data vi behöver från relevanta tabeller
+            //Lagras sedan i fälten från ShippingBillViewModel
+            var shippingBillDetails = _hatcontext.ShippingBills
+                .Where(sb => sb.ShippingBillId == id)
+                .Select(sb => new ShippingBillViewModel
+                {
+                    productCode = sb.productCode,
+                    customerFullName = sb.order.Customer.firstName + " " + sb.order.Customer.lastName,
+                    addressDetails = sb.order.Address.streetName + ", " + sb.order.Address.zipCode + " " + sb.order.Address.cityName + ", " + sb.order.Address.countryName
+                }).FirstOrDefault();
+
+
+
+            if (shippingBillDetails == null)
+            {
+                return NotFound(); // Ingen fraktsedel matchar givet ID = Returnera HTTP 404 Not Found
+            }
+
+            return View(shippingBillDetails); // Skicka data till view
+        }
+    }
+
+
+}
---
+++ Models/ShippingBillViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Grupp3Hattmakaren.Models
+{
+    public class ShippingBillViewModel
+    {
+        public string productCode { get; set; } //Behövs hårdkodas? 6504 00 00
+        public string customerFullName { get; set; }
+        public string addressDetails { get; set; }
+    }
+}
