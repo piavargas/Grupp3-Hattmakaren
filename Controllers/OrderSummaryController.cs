25667146d3112e3884a9fb1b2120f7b0f9745c31
dakipazar <132764398+dakipazar@users.noreply.github.com>
2024-04-23 12:49:08 +0200

    Fetch från enquiry istället för order

--- Controllers/OrderSummaryController.cs
+++ Controllers/OrderSummaryController.cs
@@ -24,22 +24,20 @@ namespace Grupp3Hattmakaren.Controllers
         }
 
 
-        public IActionResult PrintOrderSummary(int orderId)
+        public IActionResult PrintOrderSummary(int enquiryId)
         {
+            // Hämta enquiry och tillhörande data
+            var enquiry = _context.Enquiries
+            // Avkommentera om vi ska ha information om kunden i ordersummary:
+            //.Include(e => e.Customer)
+            //.Include(o => o.Address)
 
-            // Hämta ordern och tillhörande data
-            var order = _context.Orders
-                .Include(o => o.Customer)
-                .Include(o => o.Address)
-                .Include(o => o.products)  // Kollektion av produkter i en order 
-                .FirstOrDefault(o => o.OrderId == orderId);
+            .FirstOrDefault(e => e.EnquiryId == enquiryId);
 
-            if (order == null)
+            if (enquiry == null)
             {
                 return NotFound();
             }
-
-
 
             // Skapa en PDF
             using (MemoryStream memoryStream = new MemoryStream())
@@ -49,37 +47,21 @@ namespace Grupp3Hattmakaren.Controllers
                 doc.Open();
 
                 // Lägg till orderinformation i PDF-filen
-                doc.Add(new Paragraph($"Order ID: {order.OrderId}"));
-                doc.Add(new Paragraph($"Customer Name: {order.Customer.firstName} {order.Customer.lastName}"));
-                doc.Add(new Paragraph($"Address: {order.Address.streetName}, {order.Address.zipCode}, {order.Address.countryName}"));
-
-                //// Information om varje produkt i ordern
-                //foreach (var item in order.products)
-                //{
-                //    doc.Add(new Paragraph($"Product: {item.productName}, Price: {item.price}"));
-                //}
-
-                // Information om varje produkt i ordern
-                foreach (var item in order.products)
-                {
-                    string materialNames = item.materials != null && item.materials.Any()
-                        ? string.Join(", ", item.materials.Select(m => m.name))
-                        : "No materials";
-
-                    doc.Add(new Paragraph($"Product: {item.productName}, Materials: {materialNames}, Price: {item.price}"));
-                }
-
-
-                //Avslutningsvis lägger vi till det totala priset
-                doc.Add(new Paragraph($"Total Price: {order.price}"));
-
-
+                //doc.Add(new Paragraph($"Enquiry ID: {enquiry.EnquiryId}"));
+                doc.Add(new Paragraph($"Head Size: {enquiry.headSize}"));
+                doc.Add(new Paragraph($"Consent to Modify Existing Hat: {(enquiry.consentHat ? "Yes" : "No")}"));
+                doc.Add(new Paragraph($"Description: {enquiry.description}"));
+                doc.Add(new Paragraph($"Fabric Material: {enquiry.fabricMaterial}"));
+                doc.Add(new Paragraph($"Special Effect Materials: {enquiry.specialEffectMaterials}"));
+                doc.Add(new Paragraph($"Color: {enquiry.color}"));
+                doc.Add(new Paragraph($"Text on Hat: {enquiry.textOnHat}"));
+                doc.Add(new Paragraph($"Font: {enquiry.font}"));
 
                 doc.Close();
 
                 byte[] pdfBytes = memoryStream.ToArray();
 
-                return File(pdfBytes, "application/pdf", "order_summary.pdf");
+                return File(pdfBytes, "application/pdf", "enquiry_details.pdf");
             }
         }
 
